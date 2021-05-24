b8aa2069c15494c3bbe928ad4039438a0ca6ec54
Euan Enticott <euanenticott@gmail.com>
2021-05-24 12:56:45 +0100

    improved speed of rmvn_omp

--- code/rmvn_testing.R
+++ code/rmvn_testing.R
@@ -3,15 +3,22 @@ library(Rcpp)
 library(RcppArmadillo)
 library(microbenchmark)
 library(mvnfast)
+library(MASS)
 sourceCpp("code/rmvn_omp.cpp")
 
 
 plot(rmvn(1000, c(1, 2), sigma = matrix(c(2,2,2,4), nrow = 2)))
 plot(rmvn_omp(1000, c(1, 2), sigma = matrix(c(2,2,2,4), nrow = 2), seeds = c(1,2)))
 
-microbenchmark(rmvnomp1 = rmvn_omp(1000000, c(1, 2), sigma = matrix(c(2,2,2,4), nrow = 2), seeds = c(1)),
+microbenchmark(MASSmvrnorm = mvrnorm(1000000, c(1, 2), Sigma = matrix(c(2,2,2,4), nrow = 2)),
+               rmvnomp1 = rmvn_omp(1000000, c(1, 2), sigma = matrix(c(2,2,2,4), nrow = 2), seeds = c(1)),
                rmvnfast1 = rmvn(1000000, c(1,2), sigma = matrix(c(2,2,2,4), nrow = 2), ncores = 1),
                rmvnomp2 = rmvn_omp(1000000, c(1, 2), sigma = matrix(c(2,2,2,4), nrow = 2), seeds = c(1,2)),
                rmvnfast2 = rmvn(1000000, c(1,2), sigma = matrix(c(2,2,2,4), nrow = 2), ncores = 2),
+               rmvnomp4 = rmvn_omp(1000000, c(1, 2), sigma = matrix(c(2,2,2,4), nrow = 2), seeds = c(1,2,3,4)),
+               rmvnfast4 = rmvn(1000000, c(1,2), sigma = matrix(c(2,2,2,4), nrow = 2), ncores = 4),
+               rmvnomp8 = rmvn_omp(1000000, c(1, 2), sigma = matrix(c(2,2,2,4), nrow = 2), seeds = c(1,2,3,4,5,6,7,8)),
+               rmvnfast8 = rmvn(1000000, c(1,2), sigma = matrix(c(2,2,2,4), nrow = 2), ncores = 8),
                unit = "relative",
-               times = 100)
+               times = 50)
+
